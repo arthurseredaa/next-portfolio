ddae70ab2834329c3356159971e693e94b482237
arthurseredaa <neonbetcy@gmail.com>
2020-12-26 15:02:18 +0200

    Create Layout for pages

---
+++ components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import { Navbar } from "../shared/Navbar";
+import Head from "next/head";
+import { Hero } from "../shared/Hero";
+
+export const Layout = ({ page, showHero, showFooter, children }) => (
+  <>
+    <Head>
+      <title>{page} | .arthurseredaa</title>
+    </Head>
+    <Navbar />
+    {showHero && <Hero />}
+    <div className="container">{children}</div>
+    {showFooter && (
+      <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+        <div className="container text-center">
+          <small>Copyright &copy; Your Website</small>
+        </div>
+      </footer>
+    )}
+  </>
+);
