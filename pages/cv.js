e71860ab7d7852bd46e62702198705d288356d77
arthurseredaa <neonbetcy@gmail.com>
2020-12-29 01:23:38 +0200

    Create new pages

---
+++ pages/cv.js
@@ -0,0 +1,9 @@
+import {Layout} from "@/components/Layout/Layout";
+
+const Cv = () => (
+  <Layout>
+    <h1>Cv page in process...</h1>
+  </Layout>
+)
+
+export default Cv;
---
+++ pages/register.js
@@ -0,0 +1,9 @@
+import {Layout} from "@/components/Layout/Layout";
+
+const Register = () => (
+  <Layout>
+    <h1>Register page in process...</h1>
+  </Layout>
+)
+
+export default Register;
