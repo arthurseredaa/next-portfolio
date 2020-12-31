6a324265089adab25001dfcac6bb9328b974a75a
arthurseredaa <neonbetcy@gmail.com>
2020-12-31 09:21:10 +0200

    Fix

--- server/index.js
+++ server/index.js
@@ -31,9 +31,9 @@ app.prepare().then(() => {
   };
 
   server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   })
 
