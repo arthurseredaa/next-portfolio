f0fbad0fae7c9b44f8a257bb5f72d57c53e4c990
arthurseredaa <neonbetcy@gmail.com>
2021-01-04 10:33:39 +0200

    styles for preloader

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
-import App from "next/app";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/index.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/index.scss";
+import "@/styles/preloader.scss"
 import Head from "next/head";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000",
+  cache: new InMemoryCache()
+});
+
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link rel="icon" href={"/favicon.png"} type="image/png" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <link rel="icon" href={"/favicon.png"} type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
