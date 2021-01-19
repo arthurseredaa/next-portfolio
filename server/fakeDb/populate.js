a0bb9a804732ff428d89a7a16e08e62584a3b75d
arthurseredaa <neonbetcy@gmail.com>
2021-01-19 14:09:10 +0200

    File that reset db collection to default

---
+++ server/fakeDb/populate.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("../config/dev");
+const fakeDB = require("./FakeDb");
+
+mongoose.connect(
+  config.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  async () => {
+    console.log('Starting populating db...');
+    await fakeDB.populate();
+    await mongoose.connection.close();
+    console.log('DB has been populated');
+  }
+);
