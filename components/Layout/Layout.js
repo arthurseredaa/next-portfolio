c79ef73d3f5eb7bf7d21f19860d6728b98c81b43
arthurseredaa <neonbetcy@gmail.com>
2021-01-29 17:53:41 +0200

    UI changes

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -13,7 +13,7 @@ export const Layout = ({ page, showHero, showFooter, children }) => (
     {showFooter && (
       <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
         <div className="container text-center">
-          <small>Copyright &copy; Your Website</small>
+          <small>Copyright &copy; Arthur Sereda</small>
         </div>
       </footer>
     )}
