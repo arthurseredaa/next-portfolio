f4475d211216535e6453ba0c92cfaede3c30f185
arthurseredaa <neonbetcy@gmail.com>
2021-01-29 17:54:20 +0200

    Edit config in database and fakeDb

--- server/database/index.js
+++ server/database/index.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const config = require('../config/dev');
+
 require('./models/portfolio');
+require('./models/user');
 
 exports.init = () => {
   mongoose.connect(config.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true,
   }, () => {
     console.log('>>> Connected to database');
   })
