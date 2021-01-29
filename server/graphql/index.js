e30dc9970bc42e63663056653e096ae287d8232c
arthurseredaa <neonbetcy@gmail.com>
2021-01-29 17:56:38 +0200

    Provide user types and model in apolloserver

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
 //GraphQL Models
 const Portfolio = require("./models/Portfolio");
+const User = require("./models/User");
 //resolvers
-const { portfolioQueries, portfolioMutations } = require("./resolvers");
+const {
+  portfolioQueries,
+  portfolioMutations,
+  userMutations,
+} = require("./resolvers");
 //types
-const { portfolioType } = require("./types");
+const { portfolioType, userType } = require("./types");
 
-const {ApolloServer, gql} = require("apollo-server-express")
+const { ApolloServer, gql } = require("apollo-server-express");
 
 exports.createApolloServer = () => {
   const typeDefs = gql(`
+
   ${portfolioType}
 
+  ${userType}
+
   type Query {
-    hello: String
+    hello: String,
     portfolios: [Portfolio],
     portfolio(id: ID): Portfolio,
   }
@@ -21,13 +29,17 @@ exports.createApolloServer = () => {
   type Mutation {
     createPortfolio(input: PortfolioInput): Portfolio,
     updatePortfolio(id: ID, input: PortfolioInput): Portfolio,
-    deletePortfolio(id: ID): ID
+    deletePortfolio(id: ID): ID,
+
+    signIn: String,
+    signUp: String,
+    signOut: String
   }
 `);
 
   const resolvers = {
     Query: { ...portfolioQueries },
-    Mutation: { ...portfolioMutations },
+    Mutation: { ...portfolioMutations, ...userMutations },
   };
 
   const apolloServer = new ApolloServer({
@@ -36,6 +48,7 @@ exports.createApolloServer = () => {
     context: () => ({
       models: {
         Portfolio: new Portfolio(mongoose.model("Portfolio")),
+        User: new User(mongoose.model("User")),
       },
     }),
   });
