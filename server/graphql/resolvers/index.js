9db89343ca7664b0cd956bf3985866a468fd8cc2
arthurseredaa <neonbetcy@gmail.com>
2021-02-10 19:42:50 +0200

    Create resolvers for signUp & signOut

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,25 +1,33 @@
 exports.portfolioQueries = {
   portfolios: async (root, args, ctx) => await ctx.models.Portfolio.getAll(),
-  portfolio: async (root, { id }, ctx) => await ctx.models.Portfolio.getById(id),
+  portfolio: async (root, { id }, ctx) =>
+    await ctx.models.Portfolio.getById(id),
 };
 
 exports.portfolioMutations = {
   createPortfolio: async (root, { input }, ctx) => {
-    const createdPortfolio = ctx.models.Portfolio.create(input)
+    const createdPortfolio = ctx.models.Portfolio.create(input);
     return createdPortfolio;
   },
   updatePortfolio: async (root, { id, input }, ctx) => {
-    const updatedPortfolio = await ctx.models.Portfolio.update({_id: id}, input, {new: true})
+    const updatedPortfolio = await ctx.models.Portfolio.update(
+      { _id: id },
+      input,
+      { new: true }
+    );
     return updatedPortfolio;
   },
   deletePortfolio: async (root, { id }, ctx) => {
-    const deletedPortfolioId = await ctx.models.Portfolio.delete({_id: id})
-    return deletedPortfolioId._id
+    const deletedPortfolioId = await ctx.models.Portfolio.delete({ _id: id });
+    return deletedPortfolioId._id;
   },
 };
 
 exports.userMutations = {
   signIn: async (root, args, ctx) => ctx.models.User.signIn(),
-  signUp: async (root, args, ctx) => ctx.models.User.signUp(),
-  signOut: async (root, args, ctx) => ctx.models.User.signOut()
-}
+  signUp: async (root, { input }, ctx) => {
+    const registeredUser = await ctx.models.User.signUp(input);
+    return registeredUser._id;
+  },
+  signOut: async (root, args, ctx) => ctx.models.User.signOut(),
+};
