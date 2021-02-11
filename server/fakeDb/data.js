0f93eb0ad8ec9db24192293d157fc0060180746b
arthurseredaa <neonbetcy@gmail.com>
2021-02-12 01:27:04 +0200

    Add two users and add id's for portfolios

--- server/fakeDb/data.js
+++ server/fakeDb/data.js
@@ -1,4 +1,30 @@
+const mongoose = require("mongoose");
+
+const FIRST_ID = mongoose.Types.ObjectId();
+const SECOND_ID = mongoose.Types.ObjectId();
+
 const data = {
+  users: [
+    {
+      _id: FIRST_ID,
+      avatar: "https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png",
+      email: "test@gmail.com",
+      username: "Arthur Sereda",
+      name: "arthurseredaa",
+      description: "I am a React developer!",
+      role: "admin",
+      password: "123456"
+    },
+    {
+      _id: SECOND_ID,
+      avatar: "https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png",
+      email: "test1@gmail.com",
+      username: "Крутий чувак",
+      name: "krutiychuvak",
+      description: "I am a cool dude",
+      password: "123456"
+    }
+  ],
   portfolios: [
     {
       title: 'Job in Netcentric',
@@ -8,7 +34,8 @@ const data = {
       jobTitle: 'Engineer',
       description: 'Doing something, programing....',
       startDate: '01/01/2014',
-      endDate: '01/01/2016'
+      endDate: '01/01/2016',
+      user: FIRST_ID
     },
     {
       title: 'Job in Siemens',
@@ -18,7 +45,8 @@ const data = {
       jobTitle: 'Software Engineer',
       description: 'Responsoble for parsing framework for JSON medical data.',
       startDate: '01/01/2011',
-      endDate: '01/01/2013'
+      endDate: '01/01/2013',
+      user: FIRST_ID
     },
     {
       title: 'Work in USA',
@@ -28,7 +56,8 @@ const data = {
       jobTitle: 'Housekeeping',
       description: 'So much responsibility....Overloaaaaaad',
       startDate: '01/01/2010',
-      endDate: '01/01/2011'
+      endDate: '01/01/2011',
+      user: FIRST_ID
     }
   ]
 }
