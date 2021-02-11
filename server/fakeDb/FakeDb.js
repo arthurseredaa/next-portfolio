1ba26213b623cdb654618b6871e62b769c164fd5
arthurseredaa <neonbetcy@gmail.com>
2021-02-12 01:27:23 +0200

    Delete and create users in fake db

--- server/fakeDb/FakeDb.js
+++ server/fakeDb/FakeDb.js
@@ -1,8 +1,15 @@
-const {portfolios} = require('./data');
+const {portfolios, users} = require('./data');
 const Portfolio = require('../database/models/portfolio');
+const User = require('../database/models/user');
 class FakeDb {
-  clean = async () => await Portfolio.deleteMany({});
-  addData = async () => await Portfolio.create(portfolios);
+  clean = async () => {
+    await Portfolio.deleteMany({});
+    await User.deleteMany({});
+  };
+  addData = async () => {
+    await Portfolio.create(portfolios);
+    await User.create(users);
+  }
   populate = async () => {
     await this.clean();
     await this.addData();
